c065098f724a150cd0009faa291304fb0a370ecb
Ryan Fickenscher <rsficken@gmail.com>
2020-08-12 22:18:56 -0700

    004

---
+++ primatives.js
@@ -0,0 +1,71 @@
+const primatives = {};
+primatives.GridAxis = class {
+    static createMesh(context) {
+        const vertices = [];
+        const size = 1.8;
+        const divisions = 10;
+        const step = size / divisions;
+        const half = size / 2;
+        const colorLocation = 4;
+        const mesh = { drawMode: context.LINES, vao: context.createVertexArray() };
+        let stride;
+        let temp;
+
+        for (let i = 0; i <= divisions; i++) {
+            temp = -half + ( i * step);
+            vertices.push(temp);
+            vertices.push(half);
+            vertices.push(0.0);
+            vertices.push(0.0);
+
+            vertices.push(temp);
+            vertices.push(-half);
+            vertices.push(0.0);
+            vertices.push(1.0);
+
+            temp = half - ( i * step);
+            vertices.push(-half);
+            vertices.push(temp);
+            vertices.push(0.0);
+            vertices.push(0.0);
+
+            vertices.push(half);
+            vertices.push(temp);
+            vertices.push(0.0);
+            vertices.push(3.0);
+        }
+
+        mesh.verticesLength = 4;
+        mesh.verticesCount = vertices.length / mesh.verticesLength;
+        stride = Float32Array.BYTES_PER_ELEMENT * mesh.verticesLength;
+
+        mesh.verticesBuffer = context.createBuffer();
+        context.bindVertexArray(mesh.vao);
+        context.bindBuffer(context.ARRAY_BUFFER, mesh.verticesBuffer);
+        context.bufferData(context.ARRAY_BUFFER, new Float32Array(vertices), context.STATIC_DRAW);
+        context.enableVertexAttribArray(POSITION_LOCATION);
+        context.enableVertexAttribArray(colorLocation);
+
+        context.vertexAttribPointer(
+            POSITION_LOCATION,
+            3,
+            context.FLOAT,
+            false,
+            stride,
+            0
+        );
+        context.vertexAttribPointer(
+            colorLocation,
+            1,
+            context.FLOAT,
+            false,
+            stride,
+            Float32Array.BYTES_PER_ELEMENT * 3
+        );
+
+        context.bindVertexArray(null);
+        context.bindBuffer(gl.ARRAY_BUFFER, null);
+        context.mMeshCache["grid"] = mesh;
+        return mesh;
+    }
+}
